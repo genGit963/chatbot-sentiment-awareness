110286baa6dd0f4c142fe097e145db1ca993d8ad
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-11-29 14:25:06 +0545

    little bug fixed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 # bootstraper.py
 import uvicorn
-from src.app.app import app  # Correct path to import the FastAPI app
+from src.app.app import app  # type: ignore # Correct path to import the FastAPI app
 
 def bootstrap():
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-
 bootstrap()
